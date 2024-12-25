2a3136e1582cdbb62dda8d8d9dbbecb546752880
baschnit <baschni@gmail.com>
2024-12-25 19:08:02 +0100

    fixed some bugs and added some error codes

--- 42check.py
+++ 42check.py
@@ -1,8 +1,12 @@
-#!python3
+#!/usr/bin/env python3
 import sys
 import os
 from subprocess import check_output, CalledProcessError
 from typing import List
+
+#todo:
+# todo: warning: set(&var), if no & before var...
+# option to set of this warning
 
 TMP_FILE_NAME = "21norm"
 NORMINETTE_EXECUTABLE = "norminette"
@@ -10,18 +14,21 @@ NORMINETTE_EXECUTABLE = "norminette"
 ERROR_UNRECOGNIZED_TOKEN = "Error: Unrecognized token line "
 ERROR_UNRECOGNIZED_LINE = "Error: Unrecognized line ("
 ERROR_STRING_LITERAL_UNTERMINATED = "String literal unterminated detected at line "
+ERROR_UNEXPECTED_EOF = "Error: Unexpected EOF l."
 
 ERROR_NESTED_BRACKETS = "Error: Nested parentheses, braces or brackets are not correctly closed"
 ERROR_INVALID_PREPROCESSING_DIRECTIVE = "Invalid preprocessing directive"
 ERROR_EXTRA_TOKEN_ENDIF = "Extra tokens at end of #endif directive"
+ERROR_EXTRA_TOKEN_IFNDEF = "Extra tokens at end of #infdef directive"
 ERROR_INCLUDE_FILE_ARGUMENT = "Invalid file argument for #include directive"
+ERROR_MISSING_TOKEN_DEFINE = "No identifier after #define"
 
 ERROR_INVALID_MACRO = "Invalid macro function definition"
 ERROR_NO_SUCH_FILE = "no such file or directory"
 ERROR_NO_VALID_C_FILE = "is not valid C or C header file"
 
-ERROR_WITH_LINE_INFO = [ERROR_UNRECOGNIZED_LINE, ERROR_UNRECOGNIZED_TOKEN, ERROR_STRING_LITERAL_UNTERMINATED]
-ERROR_WITH_LINE_INFO_TO_FIND = [ERROR_NESTED_BRACKETS, ERROR_EXTRA_TOKEN_ENDIF, ERROR_INVALID_PREPROCESSING_DIRECTIVE, ERROR_INCLUDE_FILE_ARGUMENT]
+ERROR_WITH_LINE_INFO = [ERROR_UNEXPECTED_EOF, ERROR_UNRECOGNIZED_LINE, ERROR_UNRECOGNIZED_TOKEN, ERROR_STRING_LITERAL_UNTERMINATED]
+ERROR_WITH_LINE_INFO_TO_FIND = [ERROR_NESTED_BRACKETS, ERROR_MISSING_TOKEN_DEFINE, ERROR_EXTRA_TOKEN_IFNDEF, ERROR_EXTRA_TOKEN_ENDIF, ERROR_INVALID_PREPROCESSING_DIRECTIVE, ERROR_INCLUDE_FILE_ARGUMENT]
 ERROR_WITHOUT_LINE_INFO = [ERROR_INVALID_MACRO, ERROR_NO_VALID_C_FILE, ERROR_NO_SUCH_FILE]
 
 VALID_PREPROCESSOR_DIRECTIVES = ["ifndef", "endif", "define", "include"]
@@ -60,10 +67,12 @@ def get_errors_from_norminette(files):
 		error1 = find_from_list(line, ERROR_WITHOUT_LINE_INFO)
 		error2 = find_from_list(line, ERROR_WITH_LINE_INFO)
 		error3 = find_from_list(line, ERROR_WITH_LINE_INFO_TO_FIND)
-		if line[-5:] == ": OK!" or line == "":
+		if line == "":
 			continue
-		elif line[-8:] == ": Error!":
-			current_file = line[:-8]
+		elif line[-8:] == ": Error!" or line[-5:] == ": OK!":
+			if current_file != None and current_file in errors and errors[current_file] == []:
+				del errors[current_file]
+			current_file = line[:line.find(": ")]
 			if not current_file in errors:
 					errors[current_file] = []
 		elif (error1) != "":
@@ -77,7 +86,6 @@ def get_errors_from_norminette(files):
 			error = error2
 			line = line.replace("\x1b[0m", "").replace("\x1b[31m", "").strip()
 			line = line[len(error):].split(",", 1)
-			print(line)
 			line_number = int(line[0])
 			column_number = None
 			if len(line) == 2:
@@ -96,6 +104,10 @@ def get_errors_from_norminette(files):
 				line_number, column_number = get_line_error_nested_brackets(current_file)
 			elif error == ERROR_EXTRA_TOKEN_ENDIF:
 				line_number, column_number = get_line_extra_token_endif(current_file)
+			elif error == ERROR_EXTRA_TOKEN_IFNDEF:
+				line_number, column_number = get_line_extra_token_ifndef(current_file)
+			elif error == ERROR_MISSING_TOKEN_DEFINE:
+				line_number, column_number = get_line_missing_token_define(current_file)
 			elif error == ERROR_INCLUDE_FILE_ARGUMENT:
 				line_number, column_number = get_line_error_include_argument(current_file)
 			elif error == ERROR_INVALID_PREPROCESSING_DIRECTIVE:
@@ -124,6 +136,8 @@ def get_errors_from_norminette(files):
 			if not current_file in errors:
 				errors[current_file] = []
 			errors[current_file].append({"error_code": code.strip(), "error_msg": detail.strip(), "line": line_number, "column": column_number})
+	if current_file != None and current_file in errors and errors[current_file] == []:
+		del errors[current_file]
 	return errors
 
 def print_file_errors(file, errors, longest_prefix, longest_error, longest_msg):
@@ -162,6 +176,25 @@ def get_line_extra_token_endif(path):
 	for index, line in enumerate(file):
 		if line[:1] == "#":
 			if line[1:].strip()[:len("endif")] == "endif" and len(line[1:].strip()) > len("endif"):
+				return index + 1, None
+	return None, None
+
+
+def get_line_missing_token_define(path):
+	with open(path, "r", encoding="utf-8") as f:
+		file = f.read().split("\n")
+	for index, line in enumerate(file):
+		if line[:1] == "#":
+			if line[1:].strip()[:len("define")] == "define" and len(line[1:].strip()) == len("define"):
+				return index + 1, None
+	return None, None
+
+def get_line_extra_token_ifndef(path):
+	with open(path, "r", encoding="utf-8") as f:
+		file = f.read().split("\n")
+	for index, line in enumerate(file):
+		if line[:1] == "#":
+			if line[1:].strip()[:len("ifndef")] == "ifndef" and (line[1:].strip()[len("ifndef"):].strip().find(" ") != -1 or line[1:].strip()[len("ifndef"):].strip().find("\t") != -1):
 				return index + 1, None
 	return None, None
 
